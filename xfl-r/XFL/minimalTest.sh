cdd0988fe4872dfe1a1b9246e32d5a341423afbf
Sergio Giro <sergio.giro.ar@gmail.com>
2024-08-18 15:21:26 +0100

    Rename docker-compose.yml -> ci-docker-compose.yml

--- xfl-r/XFL/minimalTest.sh
+++ xfl-r/XFL/minimalTest.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+
+grep '' res/minimal-test/*.log
+
 export PGPASSWORD='123';
-dropdb -h localhost -U desyl -p 5432  xfl
-pg_restore --create -h localhost -U desyl -d postgres  -p 5432 ../Tables/minimalExample.pgsql -v
+dropdb -h "$POSTGRES_HOST" -U desyl -p 5432  xfl
+pg_restore --create -h "$POSTGRES_HOST" -U desyl -d postgres  -p 5432 ../Tables/minimalExample.pgsql -v
 
 python3 genExp.py
 python3 dexter.py -d=minimal-test -learn -dim=20 -subDim=40 -epochs=10 -batchSize=64
@@ -10,3 +13,7 @@ python3 dexter.py -d=minimal-test -validate
 python3 dexter.py -d=minimal-test -exportEpoch=10
 python3 pfastreXML.py -d=minimal-test -l=1024 -trees=60
 python3 pfastreXML.py -d=minimal-test -l=1024 -trees=30 -f
+
+
+grep '' res/minimal-test/*.log
+
--- xfl-r/ci_entrypoint.sh
+++ xfl-r/ci_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker compose --progress plain build xfl_host postgres_host
-docker compose --progress plain run xfl_host
+docker compose -f ci-docker-compose.yml --progress plain build xfl_host postgres_host
+docker compose -f ci-docker-compose.yml --progress plain run xfl_host
